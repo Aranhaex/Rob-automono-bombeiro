0f624c13a2defbee0b419f2b84a626457145220d
IAN GABRIEL DOS SANTOS MARQUES <ianaranha2@gmail.com>
2025-11-04 19:20:04 -0300

    Adicionado mais uma parte do braço

--- braco-robo.ino
+++ braco-robo.ino
@@ -1,25 +1,31 @@
-// Controle de dois motores DC com ESP32 via teclado (Monitor Serial)
-// Motor A -> D13 e D12
-// Motor B -> D14 e D27
+
 
 const int motorA1 = 13;
 const int motorA2 = 12;
 const int motorB1 = 14;
 const int motorB2 = 27;
+const int motorC1 = 26;
+const int motorC2 = 25;
 
 void setup() {
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
   pinMode(motorB2, OUTPUT);
+  pinMode(motorC1, OUTPUT);
+  pinMode(motorC2, OUTPUT);
 
   Serial.begin(115200);
+
+
   Serial.println("Controle de motores pronto!");
   Serial.println("Digite:");
   Serial.println("1 - Motor A frente");
   Serial.println("2 - Motor A trás");
   Serial.println("3 - Motor B frente");
   Serial.println("4 - Motor B trás");
+  Serial.println("5 - Motor C frente");
+  Serial.println("6 - Motor C trás");
 }
 
 void loop() {
@@ -31,14 +37,14 @@ void loop() {
       case '1':
         Serial.println("Motor A -> Frente");
         moverFrenteA();
-        delay(1000);
+        delay(125);
         pararA();
         break;
 
       case '2':
         Serial.println("Motor A -> Trás");
         moverTrasA();
-        delay(1000);
+        delay(125);
         pararA();
         break;
 
@@ -56,8 +62,22 @@ void loop() {
         pararB();
         break;
 
+        case '5':
+        Serial.println("Motor C -> Frente");
+        moverFrenteC();
+        delay(1000);
+        pararC();
+        break;
+
+      case '6':
+        Serial.println("Motor C -> Trás");
+        moverTrasC();
+        delay(1000);
+        pararC();
+        break;
+
       default:
-        Serial.println("Comando inválido. Use 1, 2, 3 ou 4.");
+        Serial.println("Comando inválido. Use 1, 2, 3, 4, 5 ou 6.");
         break;
     }
   }
@@ -93,4 +113,19 @@ void moverTrasB() {
 void pararB() {
   digitalWrite(motorB1, LOW);
   digitalWrite(motorB2, LOW);
+}
+
+void moverFrenteC() {
+  digitalWrite(motorC1, HIGH);
+  digitalWrite(motorC2, LOW);
+}
+
+void moverTrasC() {
+  digitalWrite(motorC1, LOW);
+  digitalWrite(motorC2, HIGH);
+}
+
+void pararC() {
+  digitalWrite(motorC1, LOW);
+  digitalWrite(motorC2, LOW);
 }
